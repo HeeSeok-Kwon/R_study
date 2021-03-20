f05350630964911058bdbc6337f6ec1acd5f974b
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-03-20 16:45:35 +0900

    Add files via upload

---
+++ R_kmooc1/6weeks/[R]변수_test.R
@@ -0,0 +1,36 @@
+print("hello")
+print("안녕하세요")
+##변수 이름 규칙##
+test1<-1
+test1<-10
+test = 100
+
+변수1 <- 10
+
+_test <- 10
+.num <- 10
+
+test1 <- 변수1
+9test <- 2
+
+a <- 50
+age <- 50
+b <- 170
+height <- 170
+키 <- 170
+몸무게 <- 68
+
+full_name <-"홍길동"
+
+######################################
+sum <-0
+i <-1
+while(i<=100){
+  sum <- sum +i
+  i <- i+1
+}
+print(sum)
+.libPaths()
+installed.packages()
+install.packages("randomForest")
+library(randomForest)
